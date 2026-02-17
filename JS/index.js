42e7df14ec396580dc0c9c86ed270d8d4b463ea1
HasanLabib <hasanlabib3@gmail.com>
2026-02-17 17:32:55 +0600

    smooth transition applied to marque. changing marque nodes to simple array and doubling the number of elements so that transition look smooth

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,14 @@
 const marque_div = document.querySelector("#marque-div");
-const marque_logos = marque_div.querySelectorAll("img");
-//console.log(marque_div,marque_logo)
+const marque_logos_node = marque_div.querySelectorAll("img");
+const marque_logos_arr=[...marque_logos_node]
+marque_logos_arr.push(...marque_logos_arr)
+//console.log(marque_div, marque_logos_arr);
 
-for (const marque_logo of marque_logos) {
+marque_logos_arr.forEach((marque_logo) => {
   const addMarqueLogo = document.createElement("img");
   addMarqueLogo.src = marque_logo.src;
   addMarqueLogo.alt = marque_logo.alt;
   addMarqueLogo.loading = marque_logo.loading;
   marque_div.append(addMarqueLogo);
-}
+});
+
